479ea7adbca907923fd7c1abd9e33059c7f9486f
Gustavo Fragas <gustavofragascunha@gmail.com>
2026-01-14 19:05:32 -0300

    update using zed and warp

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Hello World! This is the Journal Project");
+        Console.WriteLine("Gustavo");
+        Console.WriteLine("Fragas");
+        Console.WriteLine("This is a line added to the Journal Project");
     }
 }
